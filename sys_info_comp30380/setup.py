6792cd68d83fee021d72c94fd674e4183f66ac74
pedro-morachacon <122901168+pedro-morachacon@users.noreply.github.com>
2023-01-26 21:54:54 +0000

    First Commit Adding the files

---
+++ sys_info_comp30380/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sys_info_comp30380',
+      version='0.1',
+      description='Project to find system information',
+      url='https://github.com/pedro-morachacon/comp30830_Software_Engineering',
+      author='pedroAntonioMoraChacon',
+      author_email='pedro.morachacon@ucdconnect.ie',
+      license='',
+      packages=['sys_info_comp30380'],
+      zip_safe=False)
